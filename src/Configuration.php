52261a1a3aa4a20491dcfe0529199ee605ffdf79
rosven9856 <rosven9856@gmail.com>
2026-05-04 20:55:00 +0300

    refactor: implement factory pattern

--- src/Configuration.php
+++ src/Configuration.php
@@ -4,62 +4,20 @@ declare(strict_types=1);
 
 namespace ForgeForGitea\Configuration;
 
-use ForgeForGitea\Configuration\Reader\Reader;
-use ForgeForGitea\Configuration\Schema\SchemaWrapper;
-use Symfony\Component\Config\Definition\Processor;
-
-final class Configuration
+final readonly class Configuration
 {
     /**
-     * @var array|mixed[]
+     * @param array<array-key, mixed> $parameters
      */
-    private readonly array $parameters;
+    public function __construct(
+        private array $parameters,
+    ) {}
 
     /**
-     * @var array|mixed[]
-     */
-    private array $validatedParameters;
-
-    public function __construct(private readonly SchemaWrapper $schema, Reader $reader)
-    {
-        $this->validatedParameters = [];
-
-        $this->parameters = $reader->read();
-    }
-
-    public function getSchemaWrapper(): SchemaWrapper
-    {
-        return $this->schema;
-    }
-
-    /**
-     * @return array|mixed[]
+     * @return array<array-key, mixed>
      */
     public function getParameters(): array
     {
         return $this->parameters;
-    }
-
-    /**
-     * @return array|mixed[]
-     */
-    public function getValidatedParameters(): array
-    {
-        return $this->validatedParameters;
-    }
-
-    /**
-     * @psalm-api
-     * @return array|mixed[]
-     */
-    public function validate(): array
-    {
-        $processor = new Processor();
-        $this->validatedParameters = $processor->processConfiguration(
-            $this->getSchemaWrapper(),
-            [$this->getParameters()],
-        );
-
-        return $this->getValidatedParameters();
     }
 }
--- tests/ConfigurationTest.php
+++ tests/ConfigurationTest.php
@@ -4,77 +4,17 @@ declare(strict_types=1);
 
 namespace ForgeForGitea\Configuration;
 
-use ForgeForGitea\Configuration\Reader\PHPArrayReader;
-use ForgeForGitea\Configuration\Schema\SchemaWrapper;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(Configuration::class)]
 final class ConfigurationTest extends TestCase
 {
-    private SchemaWrapper $schema;
-
-    #[\Override]
-    protected function setUp(): void
-    {
-        $this->schema = new SchemaWrapper();
-    }
-
-    public function testGetSchemaWrapperReturnsSchemaPassedToConstructor(): void
-    {
-        $configuration = new Configuration($this->schema, new PHPArrayReader([]));
-
-        self::assertSame($this->schema, $configuration->getSchemaWrapper());
-    }
-
-    public function testGetParametersReturnsArrayFromReader(): void
+    public function testGetParametersReturnsPassedArray(): void
     {
         $parameters = ['key' => 'value', 'another' => 42];
-        $configuration = new Configuration($this->schema, new PHPArrayReader($parameters));
+        $configuration = new Configuration($parameters);
 
         self::assertSame($parameters, $configuration->getParameters());
-    }
-
-    public function testGetParametersReturnsEmptyArrayWhenReaderProvidesNone(): void
-    {
-        $configuration = new Configuration($this->schema, new PHPArrayReader([]));
-
-        self::assertSame([], $configuration->getParameters());
-    }
-
-    public function testGetValidatedParametersIsEmptyBeforeValidation(): void
-    {
-        $configuration = new Configuration($this->schema, new PHPArrayReader(['key' => 'value']));
-
-        self::assertSame([], $configuration->getValidatedParameters());
-    }
-
-    public function testValidateReturnsProcessedParameters(): void
-    {
-        $configuration = new Configuration($this->schema, new PHPArrayReader([]));
-
-        $result = $configuration->validate();
-
-        /**
-         * @psalm-suppress RedundantCondition
-         * @phpstan-ignore staticMethod.alreadyNarrowedType
-         */
-        self::assertIsArray($result);
-    }
-
-    public function testGetValidatedParametersAfterValidationReturnsProcessedResult(): void
-    {
-        $configuration = new Configuration($this->schema, new PHPArrayReader([]));
-        $validated = $configuration->validate();
-
-        self::assertSame($validated, $configuration->getValidatedParameters());
-    }
-
-    public function testValidateReturnsSameReferenceAsGetValidatedParameters(): void
-    {
-        $configuration = new Configuration($this->schema, new PHPArrayReader([]));
-        $result = $configuration->validate();
-
-        self::assertSame($result, $configuration->getValidatedParameters());
     }
 }
